8004da6bcede0623a013ddbb8c5acee34d466789
Alexgirard <girardprogramming>
2018-11-20 21:52:26 -0500

    Restructure project

---
+++ AdventOfCode2017/AdventOfCode2017/Puzzles/Day1/Day1.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+
+namespace AdventOfCode2017.Puzzles.Day1
+{
+    public static class Day1
+    {
+        public static void Solve()
+        {
+            string puzzleInput = File.ReadAllText("../../Input/Day1.txt");
+
+            var numberList = puzzleInput.Select(x => (int)Char.GetNumericValue(x)).ToArray();
+            var numberTotal = 0;
+            for (var index = 0; index < numberList.Length; index++)
+            {
+                var number = numberList[index];
+                var nextNumber = numberList[index >= numberList.Length - 1 ? 0 : index + 1];
+                if (number == nextNumber)
+                {
+                    numberTotal += number;
+                }
+            }
+            Console.WriteLine($"Day 1: {numberTotal}");
+        }
+    }
+}
