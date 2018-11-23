9235ee01aea20494c78a208f7b98cf04613393b4
Alexgirard <Me@alexgirard.ca>
2018-11-22 23:50:43 -0500

    Day 5 Part 1

--- AdventOfCode2017/Program.cs
+++ AdventOfCode2017/Program.cs
@@ -3,6 +3,7 @@ using AdventOfCode2017.Puzzles.Day1;
 using AdventOfCode2017.Puzzles.Day2;
 using AdventOfCode2017.Puzzles.Day3;
 using AdventOfCode2017.Puzzles.Day4;
+using AdventOfCode2017.Puzzles.Day5;
 
 namespace AdventOfCode2017
 {
@@ -14,6 +15,7 @@ namespace AdventOfCode2017
             Day2.Solve();
             Day3.Solve();
             Day4.Solve();
+            Day5.Solve();
             Console.ReadKey();
         }
     }
---
+++ AdventOfCode2017/Puzzles/Day5/Day5.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+
+namespace AdventOfCode2017.Puzzles.Day5
+{
+    public static class Day5
+    {
+        public static void Solve()
+        {
+            Console.WriteLine("===Day 5===");
+            var puzzleInput = File.ReadAllLines("../../../../Input/Day5.txt").ToList();
+          List<int> puzzleList = puzzleInput.Select(int.Parse).ToList();
+
+            { //Part 1
+                int counter = 0;
+                int Index = 0;
+                while (true)
+                {
+                    try
+                    {
+                        int previousIndex = Index;
+                        Index = puzzleList[Index] + Index;
+                        puzzleList[previousIndex]++;
+                    }
+                    catch (Exception ex)
+                    {
+                        Console.WriteLine($"Part 1: {counter}");
+                    }
+                    finally
+                    {
+                        counter++;
+                    }
+
+                }
+
+            }
+
+        }
+    }
+}
+
