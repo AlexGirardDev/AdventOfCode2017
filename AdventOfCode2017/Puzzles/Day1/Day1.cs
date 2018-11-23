3a36f134a1af9d6b313702addd5ddb3f24a5ae72
Alexgirard <Me@alexgirard.ca>
2018-11-22 22:01:10 -0500

    Day 1 Part 2

--- AdventOfCode2017/Puzzles/Day1/Day1.cs
+++ AdventOfCode2017/Puzzles/Day1/Day1.cs
@@ -9,24 +9,46 @@ namespace AdventOfCode2017.Puzzles.Day1
         public static void Solve()
         {
             Console.WriteLine($"===Day 1===");
-            string puzzleInput = File.ReadAllText("../../../../Input/Day1.txt");
-          
+            string puzzleInput= File.ReadAllText("../../../../Input/Day1.txt");
+            {//Part 1
 
-            
-            var numberList = puzzleInput.Select(x => (int)Char.GetNumericValue(x)).ToArray();
-            var numberTotal = 0;
-            for (var index = 0; index < numberList.Length; index++)
-            {
-                var number = numberList[index];
-                var nextNumber = numberList[index >= numberList.Length - 1 ? 0 : index + 1];
-                if (number == nextNumber)
+
+                var numberList = puzzleInput.Select(x => (int) Char.GetNumericValue(x)).ToArray();
+                var numberTotal = 0;
+                for (var index = 0; index < numberList.Length; index++)
                 {
-                    numberTotal += number;
+                    var number = numberList[index];
+                    var nextNumber = numberList[index >= numberList.Length - 1 ? 0 : index + 1];
+                    if (number == nextNumber)
+                    {
+                        numberTotal += number;
+                    }
                 }
+
+                Console.WriteLine($"Part 1: {numberTotal}");
             }
 
-            Console.WriteLine($"Part 1: {numberTotal}");
+            {//Part 2
+                var numberList = puzzleInput.Select(x => (int)Char.GetNumericValue(x)).ToArray();
+                var numberTotal = 0;
+                for (var index = 0; index < numberList.Length; index++)
+                {
+                    var number = numberList[index];
+                    var indexAway = (numberList.Length) / 2 + index;
+                    if (indexAway >= numberList.Length)
+                    {
+                        indexAway = indexAway - (numberList.Length);
+                    }
 
+                    var nextNumber = numberList[indexAway];
+                    if (number == nextNumber)
+                    {
+                        numberTotal += number;
+                    }
+                }
+
+                Console.WriteLine($"Part 1: {numberTotal}");
+            }
         }
     }
 }
