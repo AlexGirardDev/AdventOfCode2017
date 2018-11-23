b69ddd939b5240daeeaf375aebb3f48315ef3b7a
Alexgirard <Me@alexgirard.ca>
2018-11-22 21:51:11 -0500

    Day 3 Part 2

--- AdventOfCode2017/Puzzles/Day3/Day3.cs
+++ AdventOfCode2017/Puzzles/Day3/Day3.cs
@@ -13,8 +13,11 @@ namespace AdventOfCode2017.Puzzles.Day2
             Console.WriteLine("===Day 3===");
             var puzzleInput = int.Parse(File.ReadAllText("../../../../Input/Day3.txt"));
 
-            {//Part 1
-                int[,] array = new int[20_000, 20_000];//This doesnt scale if the number was very large but it was alot easier than having the array grow
+            {
+                //Part 1
+                int[,]
+                    array = new int[20_000,
+                        20_000]; //This doesnt scale if the number was very large but it was alot easier than having the array grow
                 var direction = Direction.Right;
 
 
@@ -28,7 +31,7 @@ namespace AdventOfCode2017.Puzzles.Day2
                     switch (direction)
                     {
                         case Direction.Right:
-                            array[x,y] = i;
+                            array[x, y] = i;
                             x++;
                             if (array[x, y - 1] == 0)
                                 direction = Direction.Up;
@@ -36,30 +39,100 @@ namespace AdventOfCode2017.Puzzles.Day2
                         case Direction.Up:
                             array[x, y] = i;
                             y--;
-                            if (array[x-1, y] == 0)
+                            if (array[x - 1, y] == 0)
                                 direction = Direction.Left;
                             break;
                         case Direction.Left:
                             array[x, y] = i;
                             x--;
-                            if (array[x, y+1] == 0)
+                            if (array[x, y + 1] == 0)
                                 direction = Direction.Down;
                             break;
                         case Direction.Down:
                             array[x, y] = i;
                             y++;
-                            if (array[x+1, y] == 0)
+                            if (array[x + 1, y] == 0)
                                 direction = Direction.Right;
                             break;
                     }
                 }
+
                 var answer = (10_000 - x) + (y - 10_000);
-                Console.WriteLine($"Part 2: {answer}");
-              
+                Console.WriteLine($"Part 1: {answer}");
+
             }
 
 
+            {
+                //Part 2
+                int[,]
+                    array = new int[20_000,
+                        20_000]; //This doesnt scale if the number was very large but it was alot easier than having the array grow
+                var direction = Direction.Right;
+                short x = 10_001;
+                short y = 10_000;
+                array[x, y] = 1;
+                var counter = 0;
+                var i = 0;
+                var values = new List<int>();
+                for (; i < puzzleInput;)
+                {
+                    i = GetAdjacentSum(array, x, y);
+                    values.Add(i);
+                    counter++;
+                    switch (direction)
+                    {
+                        case Direction.Right:
+                            array[x, y] = i;
+                            x++;
+                            if (array[x, y - 1] == 0)
+                                direction = Direction.Up;
+                            break;
+                        case Direction.Up:
+                            array[x, y] = i;
+                            y--;
+                            if (array[x - 1, y] == 0)
+                                direction = Direction.Left;
+                            break;
+                        case Direction.Left:
+                            array[x, y] = i;
+                            x--;
+                            if (array[x, y + 1] == 0)
+                                direction = Direction.Down;
+                            break;
+                        case Direction.Down:
+                            array[x, y] = i;
+                            y++;
+                            if (array[x + 1, y] == 0)
+                                direction = Direction.Right;
+                            break;
+                    }
+                }
+
+                Console.WriteLine($"Part 2: {i}");
+
+            }
+
         }
+
+        private static int GetAdjacentSum(int[,] array, int x, int y)
+        {
+
+            return 
+                   array[x - 0, y - 0] + //0
+                   array[x - 1, y - 1] + //1
+                   array[x - 0, y - 1] + //2
+                   array[x + 1, y - 1] + //3
+                   array[x + 1, y - 0] + //4
+                   array[x + 1, y + 1] + //5
+                   array[x + 0, y + 1] + //6
+                   array[x - 1, y + 1] + //7
+                   array[x - 1, y - 0] ; //8
+            //1 2 3
+            //8 0 4 
+            //7 6 5
+        }
+
     }
     
 
